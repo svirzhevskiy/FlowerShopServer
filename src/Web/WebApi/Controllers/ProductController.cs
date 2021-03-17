366a107ead4547244cf22d3b98391d284bfacb95
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-03-17 16:40:13 +0300

    Added ProductController

---
+++ src/Web/WebApi/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Logic;
+using Microsoft.AspNetCore.Mvc;
+using Models.Product;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service) 
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductModel>>> GetAllByCategory(Guid categoryId)
+        {
+            return await _service.GetAllByCategory(categoryId);
+        }
+    }
+}
