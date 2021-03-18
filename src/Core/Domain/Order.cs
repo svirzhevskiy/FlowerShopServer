f6de366a19957001e3aa458198343b0dcfa05e45
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-03-18 15:08:01 +0300

    Added Domain models for ordering

---
+++ src/Core/Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Order : IEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ src/Core/Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class User : IEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string Adress { get; set; }
+
+        public IEnumerable<Order> Orders { get; set; }
+
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
