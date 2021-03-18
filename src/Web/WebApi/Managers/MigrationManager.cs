a15c0b6a58c82a2515041ceae433c1bd36b93034
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-03-18 11:38:37 +0300

    Added logging of migrations

--- src/Web/WebApi/Managers/MigrationManager.cs
+++ src/Web/WebApi/Managers/MigrationManager.cs
@@ -17,18 +17,25 @@ namespace WebApi.Managers
             var services = scope.ServiceProvider;
             var context = services.GetRequiredService<TContext>();
             var logger = scope.ServiceProvider.GetRequiredService<ILogger<TContext>>();
+            var contextName = typeof(TContext).Name;
 
             try
             {
+                logger.LogInformation($"Migrating database associated with context {contextName}.");
+
                 context.Database.Migrate();
 
                 foreach (var seeder in seeders)
                 {
                     seeder(context, services, logger);
                 }
+
+                logger.LogInformation($"Database associated with context {contextName} has been migrated.");
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, $"An error occured while migrating the database used on context {contextName}.");
+
                 Console.WriteLine(ex);
             }
             finally
