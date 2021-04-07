2c10d1da2f449d19ae47fe5a2efe01a64baab149
Svirzhevskiy <Svirzhevskiy@DESKTOP-Q26LRME>
2021-04-07 14:44:43 +0300

    Added User service

---
+++ src/Core/Application/Logic/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Application.Logic
+{
+    public interface IUserService
+    {
+        public Task<bool> IsValidUserCredentials(string email, string password);
+        public Task<string> GetUserRole(string email);
+    }
+}
---
+++ src/Infrastructure/Logic/UserService.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Application.Exceptions;
+using Application.Logic;
+using Application.Services;
+using AutoMapper;
+using Database;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logic
+{
+    public class UserService : GenericEntityService<User>, IUserService
+    {
+        private readonly IHashService _hashService;
+        
+        public UserService(AppDbContext context, IMapper mapper, IHashService hashService) : base(context, mapper)
+        {
+            _hashService = hashService;
+        }
+        
+        public async Task<bool> IsValidUserCredentials(string email, string password)
+        {
+            var user = await _targetSet.FirstOrDefaultAsync(x => x.Email == email);
+
+            if (user is null)
+                throw AppError.UserNotFound;
+
+            return _hashService.Verify(password, user.Password);
+        }
+
+        public async Task<string> GetUserRole(string email)
+        {
+            var user = await _targetSet.Include(x => x.Role)
+                            .FirstOrDefaultAsync(x => x.Email == email);
+
+            if (user is null)
+                throw AppError.UserNotFound;
+
+            return user.Role.Title;
+        }
+    }
+}
